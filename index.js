1f152da557e670156f67cdf8cc238a7d3c98d86f
Robert Rotaru <rrotaru@wesleyan.edu>
2017-10-23 15:40:15 -0400

    host web client from Node

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+app.use(express.static('www'));
 var router = express.Router();
 // test route
 router.get('/', function(req, res) {
