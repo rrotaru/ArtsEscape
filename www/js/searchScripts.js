5e28fd1c99f98dcc75e6cf573dcdcb7b1fb440cd
Matthew Musich <mcr1221@hotmail.com>
2017-10-23 12:19:23 -0400

    search page and function added

---
+++ www/js/searchScripts.js
@@ -0,0 +1,33 @@
+var searchResults = [];
+
+
+$(document).ready(function(){
+    
+    $( "#searchSubmit" ).click(function() {
+        searchResults = [];
+        console.log('search started');
+
+        var searchTerm = $('#searchLastName').text();
+
+        $.ajax({
+            url: "/api/search/"+searchTerm,
+            type: "GET",
+            dataType: "json",
+            success: function(data) {
+                for (var datam in data) {
+                    searchResults.push(JSON.parse(datam));
+                }
+            }
+        });
+
+        console.log(searchResults);
+
+        for (var i = 0; i < searchResults.length; i++){
+            var d = searchResults[i];
+            $("#searchTableRows").append("<tr><td>"+d.first_name+"</td><td>"+d.last_name+"</td><td>"+d.email+"</td><td>"+d.phone+"</td><td>"+d.id+"</td></tr>");
+        }
+
+
+      });
+
+});
